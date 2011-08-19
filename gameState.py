bfb404cacee66ace00c0ad666e2450cc285f66ac
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2011-08-19 13:14:35 +0000

    forgot to add gameState.py
    git-svn-id: http://cynicsymposium.com/svn/ard@186 983af927-a833-49e5-90b5-81c778d02302

---
+++ gameState.py
@@ -0,0 +1,15 @@
+theMapName = None
+def getMapName():
+    global theMapName
+    return theMapName
+def setMapName(mapName):
+    global theMapName
+    theMapName = mapName
+    
+theGameMode = None
+def setGameMode(gameModeType):
+    global theGameMode
+    theGameMode = gameModeType()
+def getGameMode():
+    global theGameMode
+    return theGameMode
