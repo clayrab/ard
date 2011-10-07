4551789337258b589e58efdc7e996871615d910d
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2011-10-07 17:18:33 +0000

    fuck windows
    git-svn-id: http://cynicsymposium.com/svn/ard@223 983af927-a833-49e5-90b5-81c778d02302

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "SDL/SDL.h"
+// include the SDL headers you put in /mingw/include/SDL/
+
+int main(int argc, char *argv[]) {
+  // with SDL, you need the argc and argv parameters
+
+  printf("Hello world\n");
+  // with SDL, anything you printf will get printed to the
+  // file stdout.txt in the current directory, not to the screen
+}
