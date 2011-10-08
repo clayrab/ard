a8a10f3f419af2a87df5d1c3110f1ceda05562b7
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2011-10-08 21:08:03 +0000

    fuck iwndows
    git-svn-id: http://cynicsymposium.com/svn/ard@274 983af927-a833-49e5-90b5-81c778d02302

--- test.c
+++ test.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-#include "SDL/SDL.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#include <libpng12/png.h>
+#include <Python/Python.h>
+
+#include <ft2build.h>
+#include <freetype/freetype.h>
+#include <freetype/ftglyph.h>
+#include <freetype/ftoutln.h>
+#include <freetype/fttrigon.h>
+
+#include "libpngGL.h"
+#include "fonts.h"
+
 // include the SDL headers you put in /mingw/include/SDL/
 
 int main(int argc, char *argv[]){
