6343244789aa5f30d7dfef56a0903d4ac2303dba
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2011-10-08 21:32:24 +0000

    fuck iwndows
    git-svn-id: http://cynicsymposium.com/svn/ard@285 983af927-a833-49e5-90b5-81c778d02302

--- test.c
+++ test.c
@@ -19,11 +19,54 @@
 //#include "fonts.h"
 
 // include the SDL headers you put in /mingw/include/SDL/
-
-int main(int argc, char *argv[]){
-  // with SDL, you need the argc and argv parameters
-
-  printf("Hello world!!!\n");
-  // with SDL, anything you printf will get printed to the
-  // file stdout.txt in the current directory, not to the screen
+static void handleInput(){
+  SDL_Event event;
+  deltaTicks = SDL_GetTicks()-previousTick;
+  previousTick = SDL_GetTicks();
+  while(SDL_PollEvent(&event)){
+    switch(event.type){
+    case SDL_KEYDOWN:
+      if(event.key.keysym.sym == SDLK_ESCAPE){
+	done = 1;
+      }
+      break;
+    case SDL_QUIT:
+      done = 1;
+      break;
+    default:
+      break;
+    }
+  }
 }
+static void mainLoop (){
+  while ( !done ) {
+    handleInput();
+  }
+}
+int main(int argc, char **argv){
+  if ( SDL_Init (SDL_INIT_VIDEO) < 0 ) {
+    fprintf(stderr, "Couldn't initialize SDL: %s\n",SDL_GetError());
+    exit(1);
+  }
+  SDL_GL_SetAttribute (SDL_GL_DEPTH_SIZE, 16);
+  //  SDL_GL_SetAttribute (SDL_GL_DOUBLEBUFFER, 1);
+  Uint32 flags = SDL_OPENGL;
+  //flags |= SDL_FULLSCREEN;
+  gScreen = SDL_SetVideoMode (SCREEN_WIDTH, SCREEN_HEIGHT, 0, flags);
+  if (gScreen == NULL) {
+    fprintf (stderr, "Could not set OpenGL video mode: %s\n",
+	     SDL_GetError());
+    SDL_Quit();
+    exit(2);
+  }
+  int * value;
+  SDL_GL_GetAttribute(SDL_GL_DEPTH_SIZE,value);
+  printf("depth size: %d\n",*value);
+
+  SDL_ShowCursor(0);
+  //SDL_EnableUNICODE(1);
+  mainLoop();
+
+  exit(0);
+}
+
