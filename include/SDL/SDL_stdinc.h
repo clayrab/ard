30840f32bd129afa69ab6b4ba462e5c38e2a1f39
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2011-10-08 16:12:00 +0000

    fuck windows
    git-svn-id: http://cynicsymposium.com/svn/ard@234 983af927-a833-49e5-90b5-81c778d02302

--- include/SDL/SDL_stdinc.h
+++ include/SDL/SDL_stdinc.h
@@ -184,7 +184,7 @@ extern DECLSPEC void SDLCALL SDL_free(void *mem);
 
 #if defined(HAVE_ALLOCA) && !defined(alloca)
 # if defined(HAVE_ALLOCA_H)
-#  include <alloca.h>
+#  include <malloc.h>
 # elif defined(__GNUC__)
 #  define alloca __builtin_alloca
 # elif defined(_MSC_VER)
