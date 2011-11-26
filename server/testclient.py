7070cf2cd2f23e2ded39950fd51267df856e21d0
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2011-11-26 23:43:46 +0000

    SEND A UDP PACKET THRU THE NAT~~~~ WOOT~~~~
    git-svn-id: http://cynicsymposium.com/svn/ard@489 983af927-a833-49e5-90b5-81c778d02302

--- server/testclient.py
+++ server/testclient.py
@@ -3,8 +3,12 @@ import threading
 import SocketServer
 import time
 
+class MESSAGES:
+    CONNECT = 0
+    HANDSHAKE = 1    
+
 #CLIENT_IP="72.47.236.38"
-SERVER_IP="72.47.236.38"
+SERVER_IP="108.35.185.230"#manasquan
 CLIENT_LISTEN_PORT=2222
 SERVER_LISTEN_PORT=5005
 
@@ -12,5 +16,18 @@ MESSAGE="Hello, World!"
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (SERVER_IP, SERVER_LISTEN_PORT))
+sock.bind(("",CLIENT_LISTEN_PORT))
+sock.setblocking(0)
+#sock.sendto(MESSAGE, (SERVER_IP, SERVER_LISTEN_PORT))
+sock.sendto(str(MESSAGES.CONNECT), (SERVER_IP, CLIENT_LISTEN_PORT))
+while True:
+    time.sleep(1)
+    try:
+        data = sock.recv(1024)
+    except:
+        data = ''
+    if(data == str(MESSAGES.HANDSHAKE)):
+        sock.sendto(str(MESSAGES.HANDSHAKE),(SERVER_IP, CLIENT_LISTEN_PORT))
+    print "received message:", data
+                                                            
 
