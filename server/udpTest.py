505c042ffb5fedadbb46a303cb64263e5b1d7fcf
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2012-02-19 19:44:03 +0000

    time to test udp
    git-svn-id: http://cynicsymposium.com/svn/ard@553 983af927-a833-49e5-90b5-81c778d02302

---
+++ server/udpTest.py
@@ -0,0 +1,16 @@
+import socket
+import time
+import sys
+print sys.argv
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setblocking(0)
+sock.bind(("",int(sys.argv[3])))
+while True:
+    time.sleep(1)
+    print "sending 'im " + sys.argv[1] + "'"
+    sock.sendto("im " + sys.argv[1], (sys.argv[2], int(sys.argv[3])))
+    try:
+        data,address = sock.recvfrom(1024)
+    except:
+        data,address = "",""
+    print "recieved: (" + data + ")"
