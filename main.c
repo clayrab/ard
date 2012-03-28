6b1bd32786e3bf1fc40bbbb2044041327284a0a7
clay <clay@983af927-a833-49e5-90b5-81c778d02302>
2012-03-28 18:15:44 +0000

    testing
    git-svn-id: http://cynicsymposium.com/svn/ard@658 983af927-a833-49e5-90b5-81c778d02302

--- main.c
+++ main.c
@@ -38,8 +38,8 @@ static void printPyStackTrace(){
 
 #define FULL_SCREEN 1
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 960
+//#define SCREEN_WIDTH 1280
+//#define SCREEN_HEIGHT 960
 
 //#define SCREEN_WIDTH 1024
 //#define SCREEN_HEIGHT 768
