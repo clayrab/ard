00bffca1ff44b60b7bc702840f43224c8723e48f
Clayton Rabenda <claytonrabenda@gmail.com>
2013-01-08 22:19:07 +0100

    added test script

---
+++ server/testTwistd.py
@@ -0,0 +1,9 @@
+import socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "127.0.0.1"
+port = 26303
+socket.connect((host,port))
+socket.setblocking(0)
+commandLog = []
+delayedCommands = []
+socket.send("verifyVersion 0.1\r\n")
